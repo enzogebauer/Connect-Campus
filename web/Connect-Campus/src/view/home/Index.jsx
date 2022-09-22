58722e34943a9aec0ed63cb129d038ee2e5e2cbc
enzogebauer <enzo.gebauer@hotmail.com>
2022-09-21 23:40:23 -0300

    Initial commit

---
+++ web/Connect-Campus/src/view/home/Index.jsx
@@ -0,0 +1,15 @@
+import './home.css'
+import Navbar from '../../components/navbar/Navbar'
+const Index = () => {
+  return (
+
+    <section id="bg">
+      <Navbar />
+     
+      </section>
+    
+
+  )
+
+}
+export default Index
