805135c2135ccba87893252f550c6c95c4797c88
Enzo Gebauer <enzo.gebauer@aln.senaicimatec.edu.br>
2022-11-09 12:27:42 -0300

    sign e sign up

--- Connect-Campus/src/pages/Profile/Profile.jsx
+++ Connect-Campus/src/pages/Profile/Profile.jsx
@@ -3,9 +3,11 @@ export const Profile = () => {
   return (
     <>
       <Navbar />
-      <div className="w-full pt-0 h-[2000px] bg-purple-light mt-0">
-        <h1 className="pt-5 text-purple-dark text-center" >This is the Profile page</h1>
-      </div>
+      <section className="w-full pt-0 h-screen bg-purple-light mt-0 flex justify-center">
+        <div className="bg-slate w-[960px] h-[500px] rounded mt-20">
+          <h1 className="pt-5 text-purple-dark text-center" >This is the Profile page</h1>
+        </div>
+      </section>
     </>
 
   )
