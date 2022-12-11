f68965e2fb18d3a49cb0cd3b03fc8b8d01d7b902
enzogebauer <enzo.gebauer@hotmail.com>
2022-12-11 19:26:39 -0300

    atualizando codigo

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-  user:"luiz",
+  user:"zoca",
   password:"sqlazul", //mudar a senha do banco para o banco local
   host:"localhost",
   port:5432,
